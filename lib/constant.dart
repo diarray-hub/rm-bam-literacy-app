8251d613f108d328207bc6cf01b32465e5b95abe
diarray-hub <yacoudiarra.wl@gmail.com>
2024-11-26 21:35:24 +0000

    Define constants

---
+++ lib/constant.dart
@@ -0,0 +1,27 @@
+// ASR Model API URI and Authentication token
+const String asrModelApiUri = 'https://tcfqs3chnpo7tvlx.us-east-1.aws.endpoints.huggingface.cloud';
+const String asrModelApiToken = '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';
+
+const List<Map<String, String>> books = [
+  {'title': 'A-ABCDaire', 'image': 'https://drive.google.com/uc?export=download&id=1PlXg2euka1MOdrjzirpwvNObHfYRPitB'},
+  {'title': 'A-Denmisɛnya Kojuguw', 'image': 'https://drive.google.com/uc?export=download&id=1ZipgZn8Qek-AerCeIz5_ezRfsej6_vCR'},
+  {'title': 'A-Donfɛnw', 'image': 'https://drive.google.com/uc?export=download&id=15dq7fMjmhlQHkRDCnMtG7wHlrCmOwc2k'},
+  {'title': 'A-Kulɔriw', 'image': 'https://drive.google.com/uc?export=download&id=17fNMvqdd2aodu7sMZDT-dr8ra_Vg5xOH'},
+  {'title': 'B-Anw bɛ Baara Kɛ !', 'image': 'https://drive.google.com/uc?export=download&id=164WYLmhpvfixkkBHuqbEV5EYCH5J8ba5'},
+  {'title': 'B-Baganw', 'image': 'https://drive.google.com/uc?export=download&id=1PhuyPxvKfHccooq5LYWdL0AHu4GJY8MA'},
+  {'title': 'B-Bama Miirina', 'image': 'https://drive.google.com/uc?export=download&id=1rluH5P6fawYjzpvjbyAQkEqVWR3MglTD'},
+  {'title': 'B-Sɔminiminɛnw', 'image': 'https://drive.google.com/uc?export=download&id=1imPukojw8I2fOOt0N3qk2MnIqzXJwaOf'},
+  {'title': 'C-Jate', 'image': 'https://drive.google.com/uc?export=download&id=1Js17Yoq-kcKZxxj-TxUga5xSQo41VpxA'},
+  {'title': 'C-Kurun', 'image': 'https://drive.google.com/uc?export=download&id=11v4Z-6B_bGuHngLbxTndS8tu9t68XfXP'},
+  {'title': 'C-Ne ni Mama ka Gafe Kalan', 'image': 'https://drive.google.com/uc?export=download&id=1UIY5QjmQhXu3oQ4I6ZsiXzK_uQnF0Nd1'},
+  {'title': 'C-Ne ni Papa ka Gafe kalan', 'image': 'https://drive.google.com/uc?export=download&id=1XSabWhabpEcbgz1UbazLqP6GagCq4pSV'},
+  {'title': 'C-Nsbanji', 'image': 'https://drive.google.com/uc?export=download&id=12q0Hy9Q82ykBmdGesorPZCSQZfCAAb6W'},
+  {'title': 'C-Ni a tun bɛ se...', 'image': 'https://drive.google.com/uc?export=download&id=1cxqRmU8ym7LOoq8pL3dIO0MJ-wkvL3A0'},
+  {'title': 'C-Tulonkɛw', 'image': 'https://drive.google.com/uc?export=download&id=1Vk_5oFcfF4oMCEOvFMzuiXIGb5OZSG46'},
+  {'title': 'D-Fali Nalonma Ni Ba Kegunma', 'image': 'https://drive.google.com/uc?export=download&id=1Iur-D0EgQ8Xf2Acy5JUmG4v9qiPXLD6E'},
+  {'title': 'D-Kɛwale Ɲumanw', 'image': 'https://drive.google.com/uc?export=download&id=1sHCV650j34Kff1_JXWTSrJenYKWto1k2'},
+  {'title': 'D-Namasatigi', 'image': 'https://drive.google.com/uc?export=download&id=1fN2QgfHJbXF-WNz6Wwje7YFz2FaBTocD'},
+  {'title': 'D-Kogo', 'image': 'https://drive.google.com/uc?export=download&id=1somB_0K5hx9vd6lIpZW7qWWgJIl08Eg6'},
+  {'title': 'D-Sonsan Tulogɛlɛn', 'image': 'https://drive.google.com/uc?export=download&id=1jX7VgMjnlSgT8EpqSEmagZCxJSWLKRqb'},
+  {'title': 'D-Yɛlɛ ka di Npogotiginin min ye', 'image': 'https://drive.google.com/uc?export=download&id=1YcfuZBrngVBqqNtbWi1yZ8Lf1NCIywWJ'}
+];
